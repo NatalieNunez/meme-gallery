f084a9f8a2fe1b263378b744b7b1ac24295f44ed
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-02-28 23:37:11 -0800

    Rendering images step

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-var imageUrls = [];
+var gallery = document.querySelector('main');
+var imageUrls = [
+  'https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg',
+  'https://etcanada.com/wp-content/uploads/2016/06/58c0500b2ac4c342c6fc4ab096ada150.jpg?quality=80&strip=all&crop=0px%2C21px%2C500px%2C333px&resize=720%2C480',
+  'https://wiki.godvillegame.com/images/thumb/7/76/Prap.jpg/298px-Prap.jpg'
+];
 var imageUrlInput = document.querySelector('input');
 var addImageButton = document.querySelector('button');
 
@@ -9,3 +14,9 @@ addImageButton.addEventListener('click', function () {
     }
     imageUrlInput.value = '';
 });
+function updateGallery () {
+    for (let i = 0; i < imageUrls.length; i++) {
+        var imageElement = document.createElement('img');
+        imageElement.className = 'gallery-image';
+    }
+}
